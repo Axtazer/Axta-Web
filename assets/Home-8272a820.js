12f6e5eba10c036b693eaf1cc75ca4758c841d03
Axtazer <Axtazer@users.noreply.github.com>
2024-10-01 16:12:43 +0000

    deploy: a5ae29ac691119b17ce162d7c35731b2ed6c043e

---
+++ assets/Home-8272a820.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-9fc80947.js";const t={name:"Home",components:{}},c={class:"home"};function n(a,r,_,m,p,d){return o(),s("div",c," TestHome ")}const i=e(t,[["render",n]]);export{i as default};
